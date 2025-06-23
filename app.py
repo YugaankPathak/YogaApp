aa4d4edffcd692ee6a0d0c81d7ade98c8b23fc3b
Yugaank Pathak <yugaank11@gmail.com>
2025-06-23 13:22:56 +0530

    Deploy1

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from main.routes import main_bp
+import os
 
 def create_app():
-    app = Flask(__name__, static_folder="static", template_folder="main/templates")
+    app = Flask(
+        __name__,
+        static_folder="static",
+        template_folder="main/templates"
+    )
     app.register_blueprint(main_bp)
     return app
 
 if __name__ == "__main__":
-    create_app().run(debug=True)
+    
+    port = int(os.environ.get("PORT", 5000))
+    create_app().run(host="0.0.0.0", port=port, debug=True)
